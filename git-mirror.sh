566d39033b4c7364bf5cce741eb775586080bffa
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-07-22 11:29:23 +0530

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -9,7 +9,7 @@ DRY_RUN=$6
 
 # Add the token to the source and destination URLs
 SOURCE_DIR=$(basename "$SOURCE_REPO")
-if [ -z "$SOURCE_TOKEN"]
+if [ -n "$SOURCE_TOKEN" ]
 then
     SOURCE_REPO="https://${SOURCE_TOKEN}@${SOURCE_REPO#https://}"
 fi
@@ -18,34 +18,35 @@ DESTINATION_REPO="https://${DESTINATION_TOKEN}@${DESTINATION_REPO#https://}"
 
 # Debug outputs
 echo "UPDATED SOURCE=$SOURCE_REPO"
-echo "UPDATED_DESTINATION=$DESTINATION_REPO"
+echo "UPDATED DESTINATION=$DESTINATION_REPO"
 echo "DRY RUN=$DRY_RUN"
-
 
 # Execute
 git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
 git remote add mirror "$DESTINATION_REPO"
 git fetch -p mirror
+
 # Fetch all tags explicitly
-git fetch --all --tags
+git fetch origin --tags
 
-
-# Push to the new remote
-git push --mirror $TARGET_REPO_URL
-GIT_PUSH_COMMAND='git push mirror'
+# Push all refs and tags to the new remote
+GIT_PUSH_COMMAND='git push mirror --all'
 
 if [ "$DRY_RUN" = "true" ]
 then
-    GIT_PUSH_COMMAND=$GIT_PUSH_COMMAND' --dry-run'
+    GIT_PUSH_COMMAND="$GIT_PUSH_COMMAND --dry-run"
 fi
 
 if [ "$FOLLOW_TAGS" = "true" ]
 then
-    GIT_PUSH_COMMAND=$GIT_PUSH_COMMAND' --follow-tags'
+    GIT_PUSH_COMMAND="$GIT_PUSH_COMMAND --follow-tags"
+else
+    # Push tags separately if FOLLOW_TAGS is not true
+    GIT_PUSH_COMMAND="$GIT_PUSH_COMMAND && git push mirror --tags"
 fi
 
-GIT_PUSH_COMMAND=$GIT_PUSH_COMMAND' '$branch
 eval $GIT_PUSH_COMMAND
 
 # Clean up
-rm -rf "../$SOURCE_DIR"
+cd ..
+rm -rf "$SOURCE_DIR"
